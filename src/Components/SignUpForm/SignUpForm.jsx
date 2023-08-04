9d2e9bfc4d59ddb5490094fa9f03da35bcd98431
Sina Moradian <sinacsharp@gmail.com>
2023-08-04 18:29:02 +0330

    Add PhoneNumber, ConfirmPassword, and validation on mount to the formik

--- src/Components/SignUpForm/SignUpForm.jsx
+++ src/Components/SignUpForm/SignUpForm.jsx
@@ -1,7 +1,13 @@
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
-const initialValues = { name: "", email: "", password: "" };
+const initialValues = {
+  name: "",
+  email: "",
+  phoneNumber: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const onSubmit = () => {};
 
@@ -10,7 +16,22 @@ const validationSchema = Yup.object({
   email: Yup.string()
     .email("Email format is incorrect!")
     .required("Email is required!"),
-  password: Yup.string().required("Password is required!"),
+  phoneNumber: Yup.string()
+    .required()
+    .matches(
+      /^[0-9]{11}$/,
+      "Phone number must contain only numbers and has 11 number in it!"
+    )
+    .nullable(),
+  password: Yup.string()
+    .required("Password is required!")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+      "Password must contain minimum eight characters, at least one letter and one number"
+    ),
+  confirmPassword: Yup.string()
+    .required()
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
 });
 
 const SignUpForm = () => {
@@ -18,6 +39,7 @@ const SignUpForm = () => {
     initialValues,
     onSubmit,
     validationSchema,
+    validateOnMount: true,
   });
 
   console.log(formik.touched);
@@ -27,37 +49,76 @@ const SignUpForm = () => {
       className="flex flex-col w-[560px] m-auto mt-5"
       onSubmit={formik.onSubmit}
     >
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        {...formik.getFieldProps("name")}
-      />
-      {formik.errors.name && formik.touched.name && (
-        <div className="text-red-600">{formik.errors.name}</div>
-      )}
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        {...formik.getFieldProps("email")}
-      />
-      {formik.errors.email && formik.touched.email && (
-        <div className="text-red-600">{formik.errors.email}</div>
-      )}
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        {...formik.getFieldProps("password")}
-      />
-      {formik.errors.password && formik.touched.password && (
-        <div className="text-red-600">{formik.errors.password}</div>
-      )}
-      <button type="submit" className="rounded bg-slate-400 mt-5 p-1">
+      <div className="flex flex-col gap-[0.2em] my-1">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          {...formik.getFieldProps("name")}
+        />
+        {formik.errors.name && formik.touched.name && (
+          <div className="text-red-600">{formik.errors.name}</div>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-[0.2em] my-1">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          {...formik.getFieldProps("email")}
+        />
+        {formik.errors.email && formik.touched.email && (
+          <div className="text-red-600">{formik.errors.email}</div>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-[0.2em] my-1">
+        <label htmlFor="phoneNumber">Phone Number</label>
+        <input
+          type="text"
+          id="phoneNumber"
+          name="phoneNumber"
+          {...formik.getFieldProps("phoneNumber")}
+        />
+        {formik.errors.phoneNumber && formik.touched.phoneNumber && (
+          <div className="text-red-600">{formik.errors.phoneNumber}</div>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-[0.2em] my-1">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          {...formik.getFieldProps("password")}
+        />
+        {formik.errors.password && formik.touched.password && (
+          <div className="text-red-600">{formik.errors.password}</div>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-[0.2em] my-1">
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          {...formik.getFieldProps("confirmPassword")}
+        />
+        {formik.errors.confirmPassword && formik.touched.confirmPassword && (
+          <div className="text-red-600">{formik.errors.confirmPassword}</div>
+        )}
+      </div>
+
+      <button
+        type="submit"
+        className="rounded bg-slate-400 mt-5 p-1 disabled:opacity-50 disabled:cursor-not-allowed"
+        disabled={!formik.isValid}
+      >
         Sign Up!
       </button>
     </form>
